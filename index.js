5c14b9a8e6d936464559740f7582966e3f3b8a6d
RumbleFrog <xxlosttechxx@gmail.com>
2017-09-26 19:34:48 -0400

    Inquirer Question

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const Similarities = require('./lib/Similarities');
+const Table = require('cli-table2');
+const Inquirer = require('inquirer');
 
-const CIDR = new Similarities.CIDR('lol');
+// const CIDR = new Similarities.CIDR('lol');
+
+Inquirer.prompt([{type:'input',name:'ips',message:'Enter IPs: '}]).then((answers) => {
+
+})
