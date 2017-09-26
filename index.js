70744eb033ec13922b6322bf841c0eef90fbb4f9
RumbleFrog <xxlosttechxx@gmail.com>
2017-09-26 19:44:33 -0400

    Table Declaration

--- index.js
+++ index.js
@@ -2,8 +2,16 @@ const Similarities = require('./lib/Similarities');
 const Table = require('cli-table2');
 const Inquirer = require('inquirer');
 
+let table = new Table();
+
 // const CIDR = new Similarities.CIDR('lol');
 
 Inquirer.prompt([{type:'input',name:'ips',message:'Enter IPs: '}]).then((answers) => {
+  let IPs = answers.ips.split(' ');
+
+  let table = new Table({head: IPs});
+
+  let sHostname, bVPN, sCountry, sRegion, sCity, iZip = []; 
+
 
 })
