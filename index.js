a551ec58029108ea0e57a3ee4cfff30c375120be
RumbleFrog <xxlosttechxx@gmail.com>
2017-09-26 20:20:51 -0400

    Table Builder

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const Similarities = require('./lib/Similarities');
 const Table = require('cli-table2');
 const Inquirer = require('inquirer');
+const DNS = require('dns');
+const GeoIP = require('geoip-lite');
 
 let table = new Table();
 
@@ -11,7 +13,32 @@ Inquirer.prompt([{type:'input',name:'ips',message:'Enter IPs: '}]).then((answers
 
   let table = new Table({head: IPs});
 
-  let sHostname, bVPN, sCountry, sRegion, sCity, iZip = []; 
+  let sHostname, bVPN, sCountry, sRegion, sCity, iZip = [];
+
+  IPs.forEach((ip) => {
+    DNS.resolveNs(ip, (err, addresses) => {
+      sHostname.push(addresses[0]);
+      bVPN.push(new Similarities.CIDR(ip).isVPN());
+
+      let geo = GeoIP.lookup(ip);
+
+      sCountry.push(geo.country);
+      sRegion.push(geo.region);
+      sCity.push(geo.city);
+      iZip.push(geo.zip);
+    });
+  });
+
+  table.push(
+    {'Hostname': sHostname},
+    {'Is VPN': bVPN},
+    {'Country': sCountry},
+    {'Region': sRegion},
+    {'City': sCity},
+    {'Zip Code': iZip}
+  );
+
+  console.log(table.toString());
 
 
-})
+});
